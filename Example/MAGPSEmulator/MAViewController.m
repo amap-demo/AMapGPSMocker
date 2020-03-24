17186f3eddbce114390a9719d149b50eba6f5089
roylly <roylly@163.com>
2020-03-24 15:24:03 +0800

    demo:修改demo，添加位置和方向的验证逻辑

--- Example/MAGPSEmulator/MAViewController.m
+++ Example/MAGPSEmulator/MAViewController.m
@@ -8,18 +8,32 @@
 
 #import "MAViewController.h"
 #import <MAGPSEmulator/MAGPSEmulator.h>
+#import <MAGPSEmulator/MAGPSEmulatorManualMode.h>
 
-@interface MAViewController ()
+@interface MAViewController ()<MAMapViewDelegate,MAGPSEmulatorDelegate>
+
+@property (nonatomic,strong) MAGPSEmulatorManualMode *emulator;
 
 @end
 
 @implementation MAViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self initEmulator];
+    [self initMapView];
 }
+
+- (void)initEmulator {
+    self.emulator = [[MAGPSEmulatorManualMode alloc] init];
+    self.emulator.delegate = self;
+}
+
+- (void)initMapView {
+    self.mapView.showsUserLocation = YES;
+    self.mapView.delegate = self;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -27,13 +41,50 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)upClicked:(id)sender {
+//MARK: mapViewDelegate
+
+- (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
+    if (updatingLocation) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            self.emulator.startPosition = userLocation.location.coordinate;
+            self.emulator.direction = 0;
+            [self.emulator startEmulator];
+        });
+    }
 }
-- (IBAction)downClicked:(id)sender {
-}
-- (IBAction)leftClicked:(id)sender {
-}
-- (IBAction)rightClicked:(id)sender {
+//MARK: MAGPSEmulatorDelegate
+
+- (void)gpsEmulatorUpdateLocation:(CLLocation *)location {
+    NSLog(@"gpsEmulatorUpdateLocation:%@",location);
+    CLLocationCoordinate2D point = location.coordinate;
+    if (CLLocationCoordinate2DIsValid(point)) {
+        MAPointAnnotation *annotation = [[MAPointAnnotation alloc] init];
+        annotation.coordinate = point;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"new point coordinate:%f,%f",annotation.coordinate.latitude,annotation.coordinate.longitude);
+            NSLog(@"annotationCount:%d",self.mapView.annotations.count);
+            [self.mapView addAnnotation:annotation];
+        });
+    }
 }
 
+//MARK: btnAction
+
+- (IBAction)upClicked:(id)sender {
+    self.emulator.direction = 0;
+}
+- (IBAction)downClicked:(id)sender {
+    self.emulator.direction = 180;
+}
+- (IBAction)leftClicked:(id)sender {
+    self.emulator.direction = 270;
+}
+- (IBAction)rightClicked:(id)sender {
+    self.emulator.direction = 90;
+}
+
+- (IBAction)stopClicked:(id)sender {
+    [self.emulator stopEmulator];
+}
 @end
