a4de87c9b323f5d119bb4cb5f735418b8c4d60d5
roylly <roylly@163.com>
2020-03-25 11:28:01 +0800

    feat:添加方向和速度控制视图

---
+++ MAGPSEmulator/Classes/View/MADriveCarEmulatorViewController.h
@@ -0,0 +1,17 @@
+//
+//  MADriveCarEmulatorViewController.h
+//  MAGPSEmulator_Example
+//
+//  Created by lly on 2020/3/24.
+//  Copyright © 2020 xuefeng.lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MADriveCarEmulatorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
