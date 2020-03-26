a00894ce0c1114018dc9ccdcbc61799c02328dd5
roylly <roylly@163.com>
2020-03-26 15:41:38 +0800

    feat:浮框方向盘的交互问题基本解决，imageView添加手势，需要设置userInteractionEnable=YES;为了避免和dragableView的touch点拖拽事件冲突，设置手势识别器panGR.delayTouchesBegan=Yes，延迟touch事件派发，只当手势识别失败时，才派发touch事件；

--- MAGPSEmulator/Classes/View/MADriveCarEmulatorViewController.m
+++ MAGPSEmulator/Classes/View/MADriveCarEmulatorViewController.m
@@ -14,6 +14,9 @@
 @property (unsafe_unretained, nonatomic) IBOutlet UILabel *speedLabel;
 @property (unsafe_unretained, nonatomic) IBOutlet UIStepper *speedStepper;
 @property (unsafe_unretained, nonatomic) IBOutlet UIImageView *wheelImageView;
+@property (nonatomic,strong)  UIPanGestureRecognizer *panGR;
+@property (nonatomic,assign)  CGPoint startPoint;
+@property (nonatomic,assign)  CGPoint currentPoint;
 
 @end
 
@@ -23,6 +26,7 @@
     [super viewDidLoad];
     self.view.translatesAutoresizingMaskIntoConstraints = NO;
     [self initSpeedStepper];
+    [self initWheelImageView];
 }
 
 - (void)initSpeedStepper {
@@ -33,9 +37,57 @@
     [self speedStepperValueChanged:self.speedStepper];
 }
 
+- (void)initWheelImageView {
+    self.wheelImageView.userInteractionEnabled = YES;
+    self.panGR = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(wheelPan:)];
+    self.panGR.delaysTouchesBegan = YES;
+    [self.wheelImageView addGestureRecognizer:self.panGR];
+}
+
 - (IBAction)speedStepperValueChanged:(id)sender {
     self.speedLabel.text = [NSString stringWithFormat:@"%.0fkm/h",self.speedStepper.value];
 }
+
+- (void)wheelPan:(UIPanGestureRecognizer *)panGR {
+    if (panGR.state == UIGestureRecognizerStateBegan) {
+        self.startPoint = [panGR locationInView:self.wheelImageView];
+        NSLog(@"++++++pan GR begin!");
+    } else if (panGR.state == UIGestureRecognizerStateChanged) {
+        self.currentPoint = [panGR locationInView:self.wheelImageView];
+        double radian = [self rotationFromStartPoint:self.startPoint toEndPoint:self.currentPoint];
+        NSLog(@"current roattion:%f",radian * 180/M_PI);
+        self.wheelImageView.transform = CGAffineTransformRotate(CGAffineTransformIdentity, radian);
+    } else if (panGR.state == UIGestureRecognizerStateEnded) {//手势结束，同步给模拟器角度
+        self.currentPoint = [panGR locationInView:self.wheelImageView];
+        double radian = [self rotationFromStartPoint:self.startPoint toEndPoint:self.currentPoint];
+        double degree = radian * 180 /M_PI;
+        NSLog(@"final rotation:%f",degree);
+    } else if (panGR.state == UIGestureRecognizerStateCancelled) {
+        NSLog(@"事件被取消");
+        self.wheelImageView.transform = CGAffineTransformRotate(CGAffineTransformIdentity, 0);
+    }
+}
+
+/// 计算手势起点到终点，绕方向盘ImageView的旋转角度
+/// @param startPoint 起点坐标
+/// @param endPoint 终点坐标
+- (double)rotationFromStartPoint:(CGPoint)startPoint toEndPoint:(CGPoint)endPoint {
+    CGPoint centerPoint = self.wheelImageView.center;
+    startPoint = CGPointMake(startPoint.x - centerPoint.x, startPoint.y - centerPoint.y);
+    endPoint = CGPointMake(endPoint.x - centerPoint.x, endPoint.y - centerPoint.y);
+    NSLog(@"after Transport startPoint:%@,endPoint:%@",NSStringFromCGPoint(startPoint),NSStringFromCGPoint(endPoint));
+    double dianji = startPoint.x * endPoint.x + startPoint.y * endPoint.y;
+    double distanceStart = sqrt(startPoint.x * startPoint.x + startPoint.y * startPoint.y);
+    double distanceEnd = sqrt(endPoint.x * endPoint.x + endPoint.y * endPoint.y);
+    double radian = asin(dianji / (distanceStart * distanceEnd));
+    if (endPoint.x < startPoint.x) {//向左，即逆时针旋转，角度为负
+        radian = radian * -1;
+    }
+//    return radian * M_PI / 180;
+    return radian;
+}
+
+
 
 
 
