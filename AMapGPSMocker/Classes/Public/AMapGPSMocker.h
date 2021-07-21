fa40e501e5cd52c37ca855b31f91d7e768058bee
roylly <roylly@163.com>
2021-07-21 20:18:16 +0800

    feat:修改首页示例demo

---
+++ AMapGPSMocker/Classes/Public/AMapGPSMocker.h
@@ -0,0 +1,13 @@
+//
+//  AMapGPSMocker.m
+//  AMapGPSMocker
+//
+//  Created by lly on 2021/7/21.
+//
+
+#import <AMapGPSMocker/AGMSinglePointMocker.h>
+#import <AMapGPSMocker/AGMMultiPointMocker.h>
+#import <AMapGPSMocker/AGMFloatWindowManager.h>
+#import <AMapGPSMocker/AGMCaclUtil.h>
+#import <AMapGPSMocker/AGMCoordConvertUtil.h>
+#import <AMapGPSMocker/AGMMultiPointMocker.h>
--- Example/AMapGPSMocker/AGMViewController.m
+++ Example/AMapGPSMocker/AGMViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "AGMViewController.h"
-#import <AMapGPSMocker/AGMFloatWindowManager.h>
+#import <CoreLocation/CLLocation.h>
+#import <AMapGPSMocker/AMapGPSMocker.h>
 
-@interface AGMViewController ()<MAMapViewDelegate>
+@interface AGMViewController ()<MAMapViewDelegate,CLLocationManagerDelegate>
+
+@property (nonatomic,strong) CLLocationManager *locationManager;
 
 @end
 
@@ -17,6 +20,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [_locationManager requestWhenInUseAuthorization];
+    }
+    [_locationManager startUpdatingLocation];
     [self initMapView];
     [[AGMFloatWindowManager sharedManager] showFloatWindow];
 }
@@ -27,10 +36,35 @@
     self.mapView.zoomLevel = 16.0;
 }
 
+//MARK: CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    if (locations.count > 0) {
+//        locationManager回调的经纬度是WGS84坐标，但是高德地图MAMapView和苹果地图MKMapView中的坐标系是高德坐标，即GCJ02坐标；
+        CLLocationCoordinate2D coord = locations.firstObject.coordinate;
+        NSLog(@"locationManager update location:%@",[AGMCaclUtil stringFromCoord:coord]);
+        CLLocationCoordinate2D coord02 = [AGMCoordConvertUtil gcj02FromWgs84:coord];
+        NSLog(@"locationManager coord02 get location:%@",[AGMCaclUtil stringFromCoord:coord02]);
+    }
+}
+
 //MARK: mapViewDelegate
 
 - (void)mapView:(MAMapView *)mapView didUpdateUserLocation:(MAUserLocation *)userLocation updatingLocation:(BOOL)updatingLocation {
     if (updatingLocation) {
+        NSLog(@"MAMapView update user location:%@",[AGMCaclUtil stringFromCoord:userLocation.coordinate]);
+    }
+}
+
+/**
+ *  @brief 当plist配置NSLocationAlwaysUsageDescription或者NSLocationAlwaysAndWhenInUseUsageDescription，并且[CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined，会调用代理的此方法。
+    此方法实现调用后台权限API即可（ 该回调必须实现 [locationManager requestAlwaysAuthorization] ）; since 6.8.0
+ *  @param locationManager  地图的CLLocationManager。
+ */
+- (void)mapViewRequireLocationAuth:(CLLocationManager *)locationManager {
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {    
+        [locationManager requestAlwaysAuthorization];
     }
 }
 
